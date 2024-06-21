7652f45d59cfb6a93dd898cb9c62cffc2910320b
kotesh-naik <160763127+kotesh-naik@users.noreply.github.com>
2024-06-21 15:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getFormvalue() {
     //Write your code here
-var firstName = document.getElementById("fname").name;
-	var lastName = document.getElementById("lname").name;
-	var fullName =firstName+" "+lastName;
+var firstName = document.getElementById("fname").value;
+	var lastName = document.getElementById("lname").value;
+	var fullName =firstName +" "+ lastName;
 	alert(fullName);
 }
+//alert(fullNmae); document 
